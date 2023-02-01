c33d50e9782d98ba8ec21ae9132338a84c31ad46
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-01 15:43:52 +0700

    update-Code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, Suspense, lazy} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi"; 
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div>
+        <BrowserRouter>
+          <div className="flex relative dark:bg-main-dark-bg">
+            <div className="fixed right-4 bottom-4" style={{ zIndex: '1000'}}>
+              
+            </div>
+          </div>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
