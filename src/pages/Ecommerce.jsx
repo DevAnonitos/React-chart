7834a3f54e33759fc6d17d31d2f950796d9299dd
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-04 22:00:11 +0700

    update-Ecommerce

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { IoIosMore } from 'react-icons/io';
+import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
+
+import { 
+  Stacked,
+  Pie, 
+  Button, 
+  LineChart, 
+  SparkLine 
+} from '../components';
+
+import { 
+  earningData,
+  medicalproBranding, 
+  recentTransactions,
+  weeklyStats, 
+  dropdownData, 
+  SparklineAreaData,
+  ecomPieChartData 
+} from '../data/dummy';
+
+import { useStateContext } from '../contexts/ContextProvider';
+import product9 from '../data/product9.jpg';
 
 const Ecommerce = () => {
+
+
+
   return (
-    <div>Ecommerce</div>
+    <>
+      
+    </>
   )
 }
 
