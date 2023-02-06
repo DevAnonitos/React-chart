9fc1ba772af08a4c8d6a554fd51ae61705cd47ee
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-02-06 23:33:16 +0700

    update-Ecommerce

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -36,13 +36,15 @@ const Ecommerce = () => {
           className='flex flex-wrap lg:flex-nowrap justify-center'
         >
           <div
-            className='bg-white dark:text-gray-200
-            dark:bg-secondary-dark-bg h-44
-            rounded-xl w-full lg:w-80 p-8 pt-9
-            m-3 bg-hero-pattern bg-no-repeat
-            bg-cover bg-center'
+            className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center
+            '
           >
-            
+            <div className='flex justify-between items-center'>
+              <div>
+                <p className="font-bold text-gray-400">Earnings</p>
+                <p className="text-2xl">$63,448.78</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
